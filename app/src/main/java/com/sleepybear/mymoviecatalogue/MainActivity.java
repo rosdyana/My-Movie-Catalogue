44316d642a3da8d0c4d4fb7040c10cd9d796d14a
rosdyana <rosdyana.kusuma@gmail.com>
2018-12-09 17:55:10 +0800

    [-] remove unused files

--- app/src/main/java/com/sleepybear/mymoviecatalogue/MainActivity.java
+++ app/src/main/java/com/sleepybear/mymoviecatalogue/MainActivity.java
@@ -1,9 +1,7 @@
 package com.sleepybear.mymoviecatalogue;
 
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Handler;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -19,7 +17,6 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.sleepybear.mymoviecatalogue.api.APIService;
 import com.sleepybear.mymoviecatalogue.api.NetworkInstance;
@@ -30,7 +27,6 @@ import com.sleepybear.mymoviecatalogue.fragments.TrendingFragment;
 import com.sleepybear.mymoviecatalogue.fragments.UpcomingFragment;
 import com.sleepybear.mymoviecatalogue.models.genre.Genre;
 import com.sleepybear.mymoviecatalogue.models.genre.GenreModel;
-import com.sleepybear.mymoviecatalogue.models.nowplaying.NowplayingResult;
 import com.sleepybear.mymoviecatalogue.utils.ActivityUtils;
 import com.sleepybear.mymoviecatalogue.utils.utils;
 
@@ -45,13 +41,9 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private final String FRAGMENT_OTHER = "FRAGMENT_OTHER";
-    private final String FRAGMENT_HOME = "FRAGMENT_HOME";
     public static List<Genre> ourMovieGenres = new ArrayList<>();
-    private List<String> fragmentNameList = new ArrayList<>();
     private Fragment fragment;
     private TextView mToolbarTitle;
-    private boolean doubleBackToExitPressedOnce = false;
     SearchView searchView;
 
     @BindView(R.id.bottom_navigation)
@@ -85,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
                 bundle.putString("search_query", s);
                 fragment = new SearchFragment();
                 fragment.setArguments(bundle);
-                loadFragment(fragment, FRAGMENT_OTHER);
+                loadFragment(fragment);
                 return true;
             }
 
@@ -108,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
 
         mToolbarTitle.setText(getString(R.string.text_trending));
         mToolbarTitle.setTextColor(getResources().getColor(R.color.white));
-        loadFragment(new TrendingFragment(), FRAGMENT_HOME);
+        loadFragment(new TrendingFragment());
 
         mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -117,22 +109,22 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.action_popular:
                         mToolbarTitle.setText(getString(R.string.text_popular));
                         fragment = new PopularFragment();
-                        loadFragment(fragment, FRAGMENT_OTHER);
+                        loadFragment(fragment);
                         break;
                     case R.id.action_now_playing:
                         mToolbarTitle.setText(getString(R.string.text_now_playing));
                         fragment = new NowPlayingFragment();
-                        loadFragment(fragment, FRAGMENT_OTHER);
+                        loadFragment(fragment);
                         break;
                     case R.id.action_trending:
                         mToolbarTitle.setText(getString(R.string.text_trending));
                         fragment = new TrendingFragment();
-                        loadFragment(fragment, FRAGMENT_HOME);
+                        loadFragment(fragment);
                         break;
                     case R.id.action_upcoming:
                         mToolbarTitle.setText(getString(R.string.text_upcoming));
                         fragment = new UpcomingFragment();
-                        loadFragment(fragment, FRAGMENT_OTHER);
+                        loadFragment(fragment);
                         break;
                 }
                 return true;
@@ -140,36 +132,16 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void loadFragment(final Fragment fragment, String name) {
-//        final FragmentManager fragmentManager = getFragmentManager();
-        // load fragment
+    private void loadFragment(final Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, fragment);
         Log.d("ROS ", fragment.getClass().getSimpleName());
-//        final int count = fragmentManager.getBackStackEntryCount();
-//        if(name.equals(FRAGMENT_OTHER)){
-//            transaction.addToBackStack(FRAGMENT_OTHER);
-//        }
-        fragmentNameList.add(fragment.getClass().getSimpleName());
         transaction.addToBackStack(null);
         transaction.commit();
-//
-//        fragmentManager.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
-//            @Override
-//            public void onBackStackChanged() {
-//                if(fragmentManager.getBackStackEntryCount() <= count){
-//                    fragmentManager.popBackStack(FRAGMENT_OTHER, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-//                    fragmentManager.removeOnBackStackChangedListener(this);
-//                    View view = mBottomNav.findViewById(R.id.action_trending);
-//                    view.performClick();
-//                }
-//            }
-//        });
     }
 
     @Override
     public void onBackPressed() {
-        fragmentNameList.remove(fragmentNameList.size()-1);
         if (mBottomNav.getSelectedItemId() == R.id.action_trending) {
             super.onBackPressed();
             finish();
@@ -177,59 +149,8 @@ public class MainActivity extends AppCompatActivity {
             // always back to first menu
             mBottomNav.setSelectedItemId(R.id.action_trending);
         }
-//        super.onBackPressed();
-//        String currentFragmentName = MainActivity.this.getSupportFragmentManager().findFragmentById(R.id.fragment_container).getClass().getSimpleName();
-//        if(currentFragmentName.equals(TrendingFragment.class.getSimpleName())){
-//            android.os.Process.killProcess(android.os.Process.myPid());
-//            System.exit(1);
-//        } else if (currentFragmentName.equals(PopularFragment.class.getSimpleName())){
-//            mBottomNav.setSelectedItemId(R.id.action_popular);
-//        } else if (currentFragmentName.equals(UpcomingFragment.class.getSimpleName())){
-//            mBottomNav.setSelectedItemId(R.id.action_upcoming);
-//        } else if (currentFragmentName.equals(NowPlayingFragment.class.getSimpleName())){
-//            mBottomNav.setSelectedItemId(R.id.action_now_playing);
-//        }
-//        Log.d("ROS",String.valueOf(getSupportFragmentManager().getBackStackEntryCount()));
-//        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
-//            android.support.v4.app.Fragment f =
-//                    getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-//            if (f instanceof TrendingFragment) {
-//                mBottomNav.setSelectedItemId(R.id.action_trending);
-//            } else if (f instanceof PopularFragment) {
-//                mBottomNav.setSelectedItemId(R.id.action_popular);
-//            } else if (f instanceof NowPlayingFragment) {
-//                mBottomNav.setSelectedItemId(R.id.action_now_playing);
-//            } else if (f instanceof UpcomingFragment){
-//                mBottomNav.setSelectedItemId(R.id.action_upcoming);
-//            }
-//        }
-
-//        if (doubleBackToExitPressedOnce) {
-//            super.onBackPressed();
-//        }
-//
-//        this.doubleBackToExitPressedOnce = true;
-//        Toast.makeText(this, getString(R.string.action_exit), Toast.LENGTH_SHORT).show();
-//
-//        new Handler().postDelayed(new Runnable() {
-//
-//            @Override
-//            public void run() {
-//                doubleBackToExitPressedOnce = false;
-//            }
-//        }, 2000);
     }
 
-    private int getFragmentIdByName(String s) {
-        if (s.equals("PopularFragment")) {
-            return R.id.action_popular;
-        } else if (s.equals("NowPlayingFragment")) {
-            return R.id.action_now_playing;
-        } else if (s.equals("UpcomingFragment")) {
-            return R.id.action_upcoming;
-        }
-        return R.id.action_trending;
-    }
 
     public void getGenreFromServer() {
         APIService service = NetworkInstance.getRetrofitInstance().create(APIService.class);
